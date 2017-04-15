d4e903b6104928bb4ebc6ec44afd5f64b1432551
Tobias Schubert <contact@tobias-schubert.com>
2017-04-15 23:02:28 +0200

    update rules

--- index.js
+++ index.js
@@ -3,21 +3,24 @@ module.exports = {
   extends: 'eslint:recommended',
   rules: {
 
-    'indent': [1, 2, { 'SwitchCase': 1 }],
-    'eol-last': [2],
-    'no-unused-vars': [2, {'vars': 'all', 'args': 'none'}],
+    'arrow-spacing': [2, { 'before': true, 'after': true }],
     'brace-style': [2, '1tbs', { 'allowSingleLine': true }],
-    'curly': [2],
-    'keyword-spacing': [2],
     'comma-spacing': [2, { 'before': false, 'after': true }],
-    "comma-dangle": [2, "only-multiline"],
-    'space-unary-ops': [2, {'words': true, 'nonwords': false}],
-    'space-infix-ops': [2],
-    'space-before-blocks': [2, { 'functions': 'always', 'keywords': 'always', 'classes': 'always' }],
-    'quotes': [ 2, 'single' ],
+    'curly': [2],
+    'eol-last': [2],
+    'indent': [1, 2, { 'SwitchCase': 1 }],
+    'keyword-spacing': [2],
     'linebreak-style': [2, 'unix'],
-    'semi': [2, 'always'],
+    'newline-after-var': [2],
+    'newline-before-return': [2],
     'no-console': [2, { allow: ['warn', 'error', 'assert'] }],
-    'arrow-spacing': [2, { 'before': true, 'after': true }]
+    'no-unused-vars': [2, {'vars': 'all', 'args': 'none'}],
+    'one-var-declaration-per-line': [2],
+    'quotes': [ 2, 'single' ],
+    'semi': [2, 'always'],
+    'space-before-blocks': [2, { 'functions': 'always', 'keywords': 'always', 'classes': 'always' }],
+    'space-infix-ops': [2],
+    'space-unary-ops': [2, {'words': true, 'nonwords': false}],
+    "comma-dangle": [2, "only-multiline"]
   }
 };
